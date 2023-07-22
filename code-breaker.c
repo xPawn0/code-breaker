68b897f52ba9edd6bc5420ffb79adfc85c88e201
xPawn0 <xPawn0@protonmail.com>
2023-07-23 02:59:00 +0300

    added function
    Added new  function to the source file.

--- code-breaker.c
+++ code-breaker.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
+#include <locale.h>
 
 #include "argparse.h"
 
@@ -18,7 +19,8 @@ static const char *usages[] = {
     NULL,
 };
 
-int make_file_copy(FILE *origfd, char *filename);
+int copy_file(FILE *origfd, char *fn);
+void zwsp_inject(FILE *fd, char *fn);
 
 int main(int argc, const char *argv[])
 {
@@ -39,26 +41,56 @@ int main(int argc, const char *argv[])
   argparse_describe(&argparse, doc, NULL);
   argc = argparse_parse(&argparse, argc, argv);
 
-  FILE *fd = fopen(infile, "a+");
+  FILE *fd = fopen(infile, "r");
   if (MAKE_COPY) {
-    make_file_copy(fd, infile);
+    copy_file(fd, infile);
   }
 
+  zwsp_inject(fd, infile);
   return 0;
 }
 
-int make_file_copy(FILE *origfd, char *filename)
+int copy_file(FILE *origfd, char *fn)
 {
-  char *cpfilename;
-  cpfilename = strndup(filename, strlen(filename));
-  strncat(cpfilename, extension, strlen(extension)+1);
-  FILE *copyfd = fopen(cpfilename, "w+");
+  char *cpfn;
+  cpfn = strndup(fn, strlen(fn));
+  strncat(cpfn, extension, strlen(extension)+1);
+  FILE *copyfd = fopen(cpfn, "w+");
 
   char c = fgetc(origfd);
   while (c != EOF) {
     fputc(c, copyfd);
     c = fgetc(origfd);
   }
+
   fclose(copyfd);
   return 1;
+}
+
+void zwsp_inject(FILE *fd, char *fn)
+{
+  setlocale(LC_CTYPE, "");
+  wchar_t zwsp_byte = 0x200b;
+
+  char *cpfn;
+  cpfn = strndup(fn, strlen(fn));
+  strncat(cpfn, ".cp", 4);
+  FILE *copyfd = fopen(cpfn, "w+");
+
+  int stream = fwide(copyfd, 1); //wide-character oriented stream
+  if (stream < 0) {
+    fprintf(stderr, "ERROR: file '%s' is byte oriented.\n", fn);
+    exit(-2);
+  }
+
+  char c = fgetc(fd);
+  while (c != EOF) {
+    fwprintf(copyfd, L"%lc", c);
+    fwprintf(copyfd, L"%lc", zwsp_byte);
+    c = fgetc(fd);
+  }
+
+  fclose(copyfd);
+  fclose(fd);
+  rename(cpfn, fn); //rename() will remove the file named `fn`
 }
